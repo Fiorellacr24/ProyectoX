d84a68a09440e11d5aee4b54c003dac1d922e9ca
Wendy Reyes <wendy.reyessan@gmail.com>
2017-04-24 21:55:16 -0500

    subiendo slideshow

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function(event) {
+
+	moviesAccion = [{url:"assets/images/accion-1.jpg"},{url:"assets/images/accion-2.jpg"},{url:"assets/images/accion-3.jpg"}];
+	moviesTerror = [{url:"assets/images/terror-1.jpg"},{url:"assets/images/terror-2.jpg"},{url:"assets/images/terror-3.jpg"}];
+	moviesInfantil = [{url:"assets/images/infantil-1.jpg"},{url:"assets/images/infantil-2.jpg"},{url:"assets/images/infantil-3.jpg"}];
+
+});
