b29d97c5a8dcb210992ca20d10dc6e74c6476b7d
Wendy Reyes <wendy.reyessan@gmail.com>
2017-04-25 03:56:34 -0500

    subiendo maquetacion galeria

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,45 @@
 window.addEventListener("load", function(event) {
 
-	moviesAccion = [{url:"assets/images/accion-1.jpg"},{url:"assets/images/accion-2.jpg"},{url:"assets/images/accion-3.jpg"}];
-	moviesTerror = [{url:"assets/images/terror-1.jpg"},{url:"assets/images/terror-2.jpg"},{url:"assets/images/terror-3.jpg"}];
-	moviesInfantil = [{url:"assets/images/infantil-1.jpg"},{url:"assets/images/infantil-2.jpg"},{url:"assets/images/infantil-3.jpg"}];
+	var moviesAccion = [{url:"assets/img/accion-1.jpg"},{url:"assets/img/accion-2.jpg"},{url:"assets/img/accion-3.jpg"}];
+	var moviesTerror = [{url:"assets/img/terror-1.jpg"},{url:"assets/img/terror-2.jpg"},{url:"assets/img/terror-3.jpg"}];
+	var moviesInfantil = [{url:"assets/img/infantil-1.jpg"},{url:"assets/img/infantil-2.jpg"},{url:"assets/img/infantil-3.jpg"}];
+
+	//var filter = document.getElementsByClassName('filter');
+	var filter = document.getElementsByClassName('filter');
+	var grid = document.getElementById('content-grid');
+
+		filter[0].addEventListener("click", function(event) {
+			for (var i in moviesAccion) {
+				var div =	document.createElement("div");
+				div.classList.add("grid");
+				var img = document.createElement("img");
+				img.setAttribute("src", moviesAccion[i].url);
+				var mask = document.createElement("div");
+				mask.classList.add("transition");
+				var text = document.createElement("text");
+				text.classList.add("text");
+				text.innerHTML = "Sinopsis: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, placeat! Omnis iusto, cumque id excepturi. Vitae perspiciatis aut minima asperiores repellat nihil porro officiis commodi, eos accusamus voluptates, velit assumenda.";
+
+				mask.appendChild(text);
+				div.appendChild(img);
+				div.appendChild(mask);
+				grid.appendChild(div);
+			}
+		});
+
+		filter[1].addEventListener("click", function(event) {
+
+		});
+
+		filter[2].addEventListener("click", function(event) {
+
+		});
+
+
+/*
+	document.addEventListener("dragover", function(event) {
+    event.preventDefault();
+});
+*/
 
 });
